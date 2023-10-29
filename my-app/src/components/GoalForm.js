d84bf8a7ab6c1c4b5120626abcca6db28648ea94
FaizanGill <faizan220rb@gmail.com>
2023-10-29 23:09:27 +0500

    Added inline styles in form

--- my-app/src/components/GoalForm.js
+++ my-app/src/components/GoalForm.js
@@ -4,29 +4,41 @@ import { useState } from "react";
 function GoalForm(props) {
   const [input, setInput] = useState("");
   const [error, setError] = useState("");
-
+  const [isValid, setIsValid] = useState(true);
   function inputChangeHandler(event) {
+    setIsValid(true);
+    setError("");
     setInput(event.target.value);
   }
 
   function handleFormSubmit(event) {
     event.preventDefault();
-
-    if (input.trim() === "") {
-      setError("Please enter a valid goal."); // Set an error message if input is empty or contains only whitespace.
-    } else {
-      setError(""); // Reset the error message if the input is valid.
-      const newGoal = { des: input };
-      props.getNewGoal(newGoal);
-      setInput("");
+    if (input.trim().length === 0) {
+      setIsValid(false);
+      setError("Please enter a valid goal.");
+      return;
     }
+    const newGoal = { des: input };
+    props.getNewGoal(newGoal);
+    setInput("");
+    // if (input.trim() === "") {
+    //   setError("Please enter a valid goal."); // Set an error message if input is empty or contains only whitespace.
+    // } else {
+    //   setError(""); // Reset the error message if the input is valid.
+    //   const newGoal = { des: input };
+    //   props.getNewGoal(newGoal);
+    //   setInput("");
+    // }
   }
 
   return (
     <div className="goalForm">
       <form onSubmit={handleFormSubmit}>
-        <label>Course Goal</label>
+        <label style={{ color: isValid ? "black" : "red", outline: "none" }}>
+          Course Goal
+        </label>
         <input
+          style={{ borderColor: isValid ? "black" : "red" }}
           type="text"
           name="desc"
           value={input}
